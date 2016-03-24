1ea6ec6dbfc7631286154af4639a85b7c7ae6923
Andreas Ek <andreas@andreasek.se>
2016-03-24 12:35:37 +0100

    Some more design changes

--- resources/views/contents/wheel.blade.php
+++ resources/views/contents/wheel.blade.php
@@ -3,8 +3,12 @@
     <h3 class="box-title">Årshjulet</h3>
   </div><!-- /.box-header -->
   <div class="box-body">
-      Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit.<br/>
-      <img src="{{ asset('assets/images/aret.png') }}" style="width:100%" />
+      <p>
+        Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit.<br/>
+      </p>
+      <p class="text-center">
+        <img style="max-height:400px;" src="{{ asset('assets/images/aret.png') }}" style="width:100%" />
+      </p>
   </div><!-- /.box-body -->
   <div class="box-footer">
     Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
